b4a603d49dc4e72414bdbe88e9d128ad8733a315
kokos0052 <izbagov0133@gmail.com>
2021-05-31 22:44:43 +0300

    Add two methods

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 const methodEuler = document.getElementById("method-1");
 const methodRunge = document.getElementById("method-2");
 const methodIter = document.getElementById("method-3");
+const methodRectangle = document.getElementById("method-4");
+const methodTrapezoid = document.getElementById("method-5");
 
 const resultButton = document.getElementById("result_button");
 const resultValue = document.getElementById("value");
@@ -123,6 +125,86 @@ function iter(e) {
   console.log(step, equation, sction);
 }
 
+function rectang(e) {
+  e.preventDefault();
+  const equation = document.getElementById("param-1").value;
+  const methodName = document.getElementById("param-2").value;
+  const step = parseFloat(document.getElementById("param-4").value);
+
+  let section = document.getElementById("param-3").value;
+  section = section.split(",").map((x) => Number(x));
+
+  const sectionBegin = section[0];
+  const sectionEnd = section[1];
+
+  const f = (x) => eval(`${equation}`);
+
+  let xCurr = sectionBegin,
+    sum = 0,
+    i = 0,
+    res = `Ответ: `;
+
+  if (methodName === "meth-1") {
+    do {
+      sum += f(xCurr);
+      xCurr += step;
+      i++;
+    } while (xCurr < sectionEnd);
+    console.log(sum);
+    res += `${sum * step}`;
+    resultValue.innerHTML = res;
+  }
+
+  if (methodName === "meth-2") {
+    xCurr += step / 2;
+    do {
+      sum += f(xCurr);
+      xCurr += step;
+      i++;
+    } while (xCurr < sectionEnd);
+    console.log(sum);
+    res += `${sum * step}`;
+    resultValue.innerHTML = res;
+  }
+
+  if (methodName === "meth-3") {
+    do {
+      sum += f(xCurr);
+      xCurr += step;
+      i++;
+    } while (xCurr < sectionEnd + step);
+    console.log(sum);
+    res += `${sum * step}`;
+    resultValue.innerHTML = res;
+  }
+}
+
+function trapezoid(e) {
+  e.preventDefault();
+  const equation = document.getElementById("param-1").value;
+  const step = parseFloat(document.getElementById("param-3").value);
+
+  let section = document.getElementById("param-2").value;
+  section = section.split(",").map((x) => Number(x));
+
+  const sectionBegin = section[0];
+  const sectionEnd = section[1];
+
+  const f = (x) => eval(`${equation}`);
+
+  let xCurr = sectionBegin + step,
+    sum = 0,
+    i = 0,
+    res = `Ответ: `;
+
+  do {
+    sum += f(xCurr);
+    xCurr += step;
+    i++;
+  } while (xCurr < sectionEnd);
+  res += `${(f(sectionBegin) + f(xCurr) + 2 * sum) * (step / 2)}`;
+  resultValue.innerHTML = res;
+}
 // Adding event listeners on methods
 methodEuler.addEventListener("click", () => {
   resultButton.addEventListener("click", euler);
@@ -133,4 +215,38 @@ methodRunge.addEventListener("click", () => {
 
 methodIter.addEventListener("click", () => {
   resultButton.addEventListener("click", iter);
+});
+
+methodRectangle.addEventListener("click", () => {
+  params.innerHTML = `
+  <label for="param-1" id="desc-1">Введите уравнение</label>
+  <input type="text" class="param-1" id="param-1">
+  <label for="param-2" id="desc-2">Введите выберите метод</label>
+  <select  id="param-2">
+      <option value="meth-1">Метод левых прямоугольников</option>
+      <option value="meth-2">Метод центральных прямоугольников</option>
+      <option value="meth-3">Метод правых прямоугольников</option>
+  </select>
+  <label for="param-3" id="desc-3">Введите отрезок</label>
+  <input type="text" class="param-3" id="param-3">
+  <label for="param-4" id="desc-4">Введите шаг</label>
+  <input type="text" class="param-4" id="param-4">
+  <button id="new_result_button">Узнать ответ</button>
+  `;
+  const newResultButton = document.getElementById("new_result_button");
+  newResultButton.addEventListener("click", rectang);
+});
+
+methodTrapezoid.addEventListener("click", () => {
+  params.innerHTML = `
+  <label for="param-1" id="desc-1">Введите уравнение</label>
+  <input type="text" class="param-1" id="param-1">
+  <label for="param-2" id="desc-2">Введите отрезок</label>
+  <input type="text" class="param-2" id="param-2">
+  <label for="param-3" id="desc-3">Введите шаг</label>
+  <input type="text" class="param-3" id="param-3">
+  <button id="new_result_button">Узнать ответ</button>
+  `;
+  const newResultButton = document.getElementById("new_result_button");
+  newResultButton.addEventListener("click", trapezoid);
 });
