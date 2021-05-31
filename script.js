a70ce0f5ebde81ea0fc1e632053b7822c1755352
kokos0052 <izbagov0133@gmail.com>
2021-05-31 09:33:57 +0300

    Added method runge

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 // Methods intilasation
-const methodEuler = document.getElementById('method-1');
-const methodRunge = document.getElementById('method-2');
+const methodEuler = document.getElementById("method-1");
+const methodRunge = document.getElementById("method-2");
 
-const resultButton = document.getElementById('result_button');
-const resultValue = document.querySelector('.value');
+const resultButton = document.getElementById("result_button");
+const resultValue = document.getElementById("value");
 // Labels intilasation
-const label_1 = document.getElementById('desc-1');
-const label_2 = document.getElementById('desc-2');
-const label_3 = document.getElementById('desc-3');
-const label_4 = document.getElementById('desc-4');
+const label_1 = document.getElementById("desc-1");
+const label_2 = document.getElementById("desc-2");
+const label_3 = document.getElementById("desc-3");
+const label_4 = document.getElementById("desc-4");
+
+const params = document.getElementById("params");
 
 //redeclared all math functions
 pow = (a, b) => Math.pow(a, b);
@@ -23,12 +25,12 @@ cos = (x) => Math.cos(x);
 // Euler function
 function euler(e) {
   e.preventDefault();
-  const equation = document.getElementById('param-1').value;
-  const y0 = parseFloat(document.getElementById('param-2').value);
-  const step = parseFloat(document.getElementById('param-4').value);
+  const equation = document.getElementById("param-1").value;
+  const y0 = parseFloat(document.getElementById("param-2").value);
+  const step = parseFloat(document.getElementById("param-4").value);
 
-  let section = document.getElementById('param-3').value;
-  section = section.split(',').map((x) => Number(x));
+  let section = document.getElementById("param-3").value;
+  section = section.split(",").map((x) => Number(x));
 
   const sectionBegin = section[0];
   const sectionEnd = section[1];
@@ -56,12 +58,46 @@ function euler(e) {
 // runge function
 function runge(e) {
   e.preventDefault();
+  const equation = document.getElementById("param-1").value;
+  const y0 = parseFloat(document.getElementById("param-2").value);
+  const step = parseFloat(document.getElementById("param-4").value);
+
+  let section = document.getElementById("param-3").value;
+  section = section.split(",").map((x) => Number(x));
+
+  const sectionBegin = section[0];
+  const sectionEnd = section[1];
+
+  const f = (x, y) => eval(`${equation}`);
+  let delX,
+    xCurr = sectionBegin,
+    yCurr = y0,
+    k1,
+    k2,
+    k3,
+    k4,
+    i = 0,
+    res = `<p>Ответ:</p>`;
+
+  do {
+    delX = xCurr + step / 2;
+    k1 = f(xCurr, yCurr);
+    k2 = f(delX, yCurr + (step / 2) * k1);
+    k3 = f(delX, yCurr + (step / 2) * k2);
+    k4 = f(xCurr + step, yCurr + step * k3);
+    yCurr += (step / 6) * (k1 + 2 * k2 + 2 * k3 + k4);
+    xCurr += step;
+    i++;
+    res += `<p>x${i} = ${xCurr} y${i} = ${yCurr}</p>`;
+  } while (xCurr < sectionEnd);
+  console.log(xCurr, yCurr);
+  resultValue.innerHTML = res;
 }
 
 // Adding event listeners on methods
-methodEuler.addEventListener('click', () => {
-  resultButton.addEventListener('click', euler);
+methodEuler.addEventListener("click", () => {
+  resultButton.addEventListener("click", euler);
 });
-methodRunge.addEventListener('click', () => {
-  resultButton.addEventListener('click', runge);
+methodRunge.addEventListener("click", () => {
+  resultButton.addEventListener("click", runge);
 });
